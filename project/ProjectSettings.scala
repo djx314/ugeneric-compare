a962d0d853b1f006bb390d9b4f94920b72cd0d2f
djx314 <djx314@sina.cn>
2020-07-10 09:30:44 +0800

    Update version.

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 object ProjectSettings {
 
   val commonSettings = List(
-    scalaVersion := "2.13.2",
+    scalaVersion := "2.13.3",
     scalacOptions ++= Seq("-feature", "-deprecation")
   )
 
